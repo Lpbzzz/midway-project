69e0f6638cc57108711e1343f9486c857eb40d84
lpb <462570646@qq.com>
2022-04-18 15:57:39 +0800

    init

---
+++ src/controller/user.ts
@@ -0,0 +1,28 @@
+export interface User {
+  id: number;
+  name: string;
+  age: number;
+}
+import { Body, Controller, Get, Post, Query } from '@midwayjs/decorator';
+
+@Controller('/api/user')
+export class UserController {
+  @Get('/')
+  async getUser(@Query('id') id: string): Promise<User> {
+    console.log(id);
+    return {
+      id: Number(id),
+      name: 'John',
+      age: 18,
+    };
+  }
+  @Post('/')
+  async getUserPost(@Body('id') key: User): Promise<User> {
+    console.log('key', key);
+    return {
+      id: Number(key) + 999,
+      name: 'John',
+      age: 18,
+    };
+  }
+}
